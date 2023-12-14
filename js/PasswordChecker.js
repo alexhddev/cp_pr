639a5c0a0e248e2c969b39defcaa2f2982890800
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-12-14 13:54:51 +0200

    Add password checker function

---
+++ js/PasswordChecker.js
@@ -0,0 +1,20 @@
+export function checkPassword(password) {
+    if (typeof password !== 'string') {
+        throw new Error('Password must be a string');
+    }
+    if (password.length < 8) {
+        throw new Error('Password must be at least 8 characters');
+    }
+    if (!password.match(/[a-z]/)) {
+        throw new Error('Password must contain at least one lowercase letter');
+    }
+    if (!password.match(/[A-Z]/)) {
+        throw new Error('Password must contain at least one uppercase letter');
+    }
+    if (!password.match(/[0-9]/)) {
+        throw new Error('Password must contain at least one number');
+    }
+    if (!password.match(/[^a-zA-Z0-9]/)) {
+        throw new Error('Password must contain at least one special character');
+    }
+}
